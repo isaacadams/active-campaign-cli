1faf675e0306231dd9ac3ef7d7c7ca76bd2026b4
Isaac Adams <isaacdadams@pm.me>
2023-05-13 21:53:21 -0400

    alpha init

---
+++ src/client.rs
@@ -0,0 +1,143 @@
+use reqwest::{blocking::Body, header, StatusCode};
+
+/// https://developers.activecampaign.com/reference/overview
+pub fn init() -> ActiveCampaignApiClient {
+    ActiveCampaignApiClient::default()
+}
+
+fn init_client() -> reqwest::blocking::Client {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        "Api-Token",
+        crate::config::load_env_var("ACTIVECAMPAIGN_API_KEY")
+            .parse()
+            .expect("failing to build active campaign client"),
+    );
+
+    reqwest::blocking::ClientBuilder::new()
+        .default_headers(headers)
+        .build()
+        .unwrap()
+}
+
+pub struct ActiveCampaignApiClient {
+    client: reqwest::blocking::Client,
+    base_url: String,
+}
+
+impl Default for ActiveCampaignApiClient {
+    fn default() -> Self {
+        Self {
+            client: init_client(),
+            base_url: crate::config::load_env_var("ACTIVECAMPAIGN_API_BASE_URL"),
+        }
+    }
+}
+
+impl ActiveCampaignApiClient {
+    /// https://developers.activecampaign.com/reference/list-all-contacts
+    pub fn find_contact_by_email(
+        &self,
+        email: &str,
+    ) -> Result<reqwest::blocking::Response, reqwest::Error> {
+        self.client
+            .get(format!("{}/contacts?email={}", self.base_url, email))
+            .send()
+    }
+
+    /// https://developers.activecampaign.com/reference/create-a-new-contact
+    pub fn create_contact<T: Into<Body>>(
+        &self,
+        payload: T,
+    ) -> Result<reqwest::blocking::Response, reqwest::Error> {
+        self.client
+            .post(format!("{}/contacts", self.base_url))
+            .body(payload)
+            .send()
+    }
+
+    /// https://developers.activecampaign.com/reference/delete-contact
+    pub fn delete_contact(&self, id: &str) -> Result<reqwest::blocking::Response, reqwest::Error> {
+        self.client
+            .delete(format!("{}/contacts/{}", self.base_url, id))
+            .send()
+    }
+}
+
+pub fn find_and_delete_by_email(
+    client: &ActiveCampaignApiClient,
+    email: &str,
+) -> Result<(), reqwest::Error> {
+    let response = client.find_contact_by_email(email)?;
+
+    let data = match response.status() {
+        StatusCode::OK => response.json::<serde_json::Value>().unwrap(),
+        _ => {
+            println!("request failed: {}", response.status());
+            if let Ok(text) = response.text() {
+                println!("{}", text);
+            }
+            return Ok(());
+        }
+    };
+
+    let id = match data["contacts"][0]["id"].as_str() {
+        Some(id) => id,
+        _ => {
+            println!("{} could not be found", email);
+            return Ok(());
+        }
+    };
+
+    client.delete_contact(id)?;
+
+    println!("{} was deleted!", email);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::models::*;
+    use reqwest::StatusCode;
+
+    #[test]
+    fn find_contact_by_email() {
+        let client = init();
+        let response = client.find_contact_by_email("test@spotpet.com").unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[test]
+    fn create_and_delete_contact() {
+        let client = init();
+
+        let contact = Contact::default();
+
+        // if contact already exists, then delete
+        find_and_delete_by_email(&client, &contact.email).unwrap();
+
+        let payload = contact.to_request().unwrap();
+        let response = client.create_contact(payload).unwrap();
+
+        match response.status() {
+            StatusCode::CREATED => {
+                assert!(true);
+                let data = response.json::<serde_json::Value>().unwrap();
+
+                // delete the new contact for cleanup
+                if let Some(id) = data["contact"]["id"].as_str() {
+                    assert!(
+                        client.delete_contact(id).is_ok(),
+                        "failed to delete the contact in cleanup phase"
+                    );
+                }
+            }
+            _ => {
+                println!("{:#?}", response.text());
+                assert!(false);
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod client;
+mod config;
+mod error;
+mod models;
+
+fn main() {
+    println!("Hello, world!");
+}
